4a8ddcd139c02dafed35bfdf4718f9f45968cb82
Cromulence <cromulence@users.noreply.github.com>
2023-11-14 19:02:37 +0000

    Update scripts to load env file

--- _service/down.sh
+++ _service/down.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker-compose -f ../_shared/docker-compose.base.yml -f ./docker-compose.yml down
+docker-compose --env-file ./.env -f ../_shared/docker-compose.base.yml -f ./docker-compose.yml down
